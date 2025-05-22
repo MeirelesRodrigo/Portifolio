cb2cdae5d6e0a44c2d1b5b403cb977fa157b9904
Rodrigo <meireles.rsilva22@gmail.com>
2025-05-22 12:35:17 -0300

    Adiconando novo projeto e corrigindo textos

--- src/app/components/projects-section/projects-section.component.ts
+++ src/app/components/projects-section/projects-section.component.ts
@@ -1,15 +1,32 @@
 import { AfterViewInit, Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faJs,
+  faNode,
+  faPython,
+  faAngular,
+  faReact,
+  faHtml5,
+  faCss3,
+  faSass,
+  faFigma,
+} from '@fortawesome/free-brands-svg-icons';
+
 import * as AOS from 'aos';
 
 @Component({
   selector: 'app-projects-section',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './projects-section.component.html',
   styleUrl: './projects-section.component.css'
 })
 export class ProjectsSectionComponent implements AfterViewInit {
+  faAngular = faAngular
+  faHtml5 = faHtml5
+  
   ngAfterViewInit(): void {
         AOS.init();
   }
+
 
 }
